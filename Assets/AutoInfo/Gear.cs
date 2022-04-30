cdca80ab000f648d5bd1179df009d2bedfbbbe20
kenosej <kenosej.business@gmail.com>
2022-04-30 21:15:59 +0200

    add data-transfer object for a gear

---
+++ Assets/AutoInfo/Gear.cs
@@ -0,0 +1,13 @@
+namespace AutoInfo
+{
+    public class Gear
+    {
+        public int Level { get; set; }
+        public float MaxSpeed { get; set; }
+        public float MaxSpeedKmh { get; set; }
+        public float LowestTorque { get; set; }
+        public float HighestTorque { get; set; }
+        public float Numerator { get; set; }
+        public float Denominator { get; set; }
+    }
+}
