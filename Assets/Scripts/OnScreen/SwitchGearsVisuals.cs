b316b554a5a6e7b901c8530dd1c56b163ed06698
kenosej <kenosej.business@gmail.com>
2022-05-09 23:45:12 +0200

    use new gear visuals

--- Assets/Scripts/OnScreen/SwitchGearsVisuals.cs
+++ Assets/Scripts/OnScreen/SwitchGearsVisuals.cs
@@ -10,13 +10,40 @@ namespace OnScreen
         public GameObject carObjReference;
         private readonly Image[] _gears = new Image[10];
 
+        private Sprite[] _unselectedGears = new Sprite[10];
+        private Sprite[] _selectedGears = new Sprite[10];
+
         private void Awake()
         {
             _pC = carObjReference.GetComponent<ParentControl>();
 
             LinkVisuals();
+            DeactivateUnusedGears();
+            LoadSprites();
+        }
+
+        private void LoadSprites()
+        {
+            _selectedGears[0] = Resources.Load<Sprite>($"SelectedGears/sn");
+            _unselectedGears[0] = Resources.Load<Sprite>($"UnselectedGears/un");
+            _selectedGears[9] = Resources.Load<Sprite>($"SelectedGears/sr");
+            _unselectedGears[9] = Resources.Load<Sprite>($"UnselectedGears/ur");
+            
+            for (int i = 1; i <= _pC.Car.NumberOfGears; i++)
+            {
+                _selectedGears[i] = Resources.Load<Sprite>($"SelectedGears/s{i}");
+                _unselectedGears[i] = Resources.Load<Sprite>($"UnselectedGears/u{i}");
+            }
         }
         
+        private void DeactivateUnusedGears()
+        {
+            for (int i = _pC.Car.NumberOfGears + 1; i < 9; i++)
+            {
+                _gears[i].enabled = false;
+            }
+        }
+
         private void LinkVisuals()
         {
             _gears[0] = transform.Find("GearNeutral").gameObject.GetComponent<Image>();
@@ -31,15 +58,18 @@ namespace OnScreen
 
         private void Update()
         {
-            ColorCurrentGear();
+            SwitchToCurrentGear();
         }
 
-        private void ColorCurrentGear()
+        private void SwitchToCurrentGear()
         {
-            foreach (var gear in _gears)
-                gear.color = Color.white;
+            for (int i = 0; i <= _pC.Car.NumberOfGears; i++)
+                _gears[i].sprite = _unselectedGears[i];
             
-            _gears[(int)_pC.CurrentGear].color = Color.black;
+            // reverse
+            _gears[9].sprite = _unselectedGears[9];
+
+            _gears[(int)_pC.CurrentGear].sprite = _selectedGears[(int)_pC.CurrentGear];
         }
     }
 }
