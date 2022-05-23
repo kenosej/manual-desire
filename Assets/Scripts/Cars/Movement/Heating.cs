1fc4dc2fbec046435010f6f757c2326c31fe8451
kenosej <kenosej.business@gmail.com>
2022-05-23 20:02:26 +0200

    change engine heating

--- Assets/Scripts/Cars/Movement/Heating.cs
+++ Assets/Scripts/Cars/Movement/Heating.cs
@@ -19,18 +19,24 @@ namespace Cars.Movement
 
         private void RegulateHeat() // TODO: manage when car is turned off
         {
-            var multiplier = _pC.Heat > 50f ? 0.75f : 1f;
-            var shouldCoolDownOnModerateRPMs = _pC.Heat > 50f;
+            // philosophy (if the car is turned on):
+            // if RPMs going up -> the higher the temp., the slower the heating
+            // if RPMs going down -> the higher the temp., the faster the cool down
+            
+            var heatStep = _pC.RPMNeedle01Position < 0.65f ? 0.02f : Mathf.Lerp(0.02f, 0.1f, _pC.RPMNeedle01Position);
+            
+            if (!_pC.IsTurnedOn || (_pC.Heat > 50f && _pC.RPMNeedle01Position < 0.65f))
+                heatStep *= -1f;
+            
+            // if RPMs are greater than 65% -> the greater the heat, the smaller the multiplier
+            // if RPMs are smaller than 65% -> the greater the heat, the bigger the multiplier
+            var multiplier = !_pC.IsTurnedOn ? 3f : Mathf.Lerp(
+                _pC.RPMNeedle01Position < 0.65f ? 0.5f : 2f,
+                _pC.RPMNeedle01Position < 0.65f ? 2f : 0.5f,
+                Mathf.InverseLerp(0f, 100f, _pC.Heat)
+                );
 
-            if (_pC.RPMNeedle01Position < 0.65f)
-                if (shouldCoolDownOnModerateRPMs)
-                    _pC.Heat -= 0.02f * multiplier;
-                else
-                    _pC.Heat += 0.02f * multiplier;
-            else if (_pC.RPMNeedle01Position < 0.75f)
-                _pC.Heat += 0.05f * multiplier;
-            else
-                _pC.Heat += 0.1f * multiplier;
+            _pC.Heat += heatStep * multiplier;
         }
     }
 }
